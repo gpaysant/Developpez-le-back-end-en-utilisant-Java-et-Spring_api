285159f30aa1a29d381868deb15fbfd555cec1b6
Gerald P <thegege2005@yahoo.fr>
2024-01-25 19:00:23 +0100

    Initialize project with spring security

---
+++ src/main/java/com/openclassrooms/api/controllers/LoginController.java
@@ -0,0 +1,26 @@
+package com.openclassrooms.api.controllers;
+
+import org.springframework.security.core.Authentication;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.openclassrooms.api.services.JWTService;
+
+
+@RestController
+public class LoginController {
+
+
+    private JWTService jwtService;
+
+    public LoginController(JWTService jwtService) {
+        this.jwtService = jwtService;
+    }
+
+    @PostMapping("/login")
+    public String getToken(Authentication authentication) {
+        String token = jwtService.generateToken(authentication);
+        return token;
+    }
+
+}
