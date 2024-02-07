82578a228a455320a82fcb865b07c0ae4492a448
Gerald P <thegege2005@yahoo.fr>
2024-02-07 01:18:09 +0100

    Add route get and get id  in controller Add method to get all rentals

--- src/main/java/com/openclassrooms/api/dto/RentalDto.java
+++ src/main/java/com/openclassrooms/api/dto/RentalDto.java
@@ -1,6 +1,10 @@
 package com.openclassrooms.api.dto;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.util.Date;
 
@@ -13,7 +17,11 @@ public class RentalDto {
     private String price;
     private String picture;
     private String description;
+    @JsonIgnore
     private UserDto userDto;
+    @JsonProperty(access = JsonProperty.Access.READ_ONLY)
+    private Long owner_id;
     private Date created_at;
     private Date updated_at;
+
 }
--- src/main/java/com/openclassrooms/api/services/RentalService.java
+++ src/main/java/com/openclassrooms/api/services/RentalService.java
@@ -4,9 +4,14 @@ import com.openclassrooms.api.dto.RentalDto;
 import com.openclassrooms.api.models.Rental;
 
 import java.text.ParseException;
+import java.util.List;
 
 public interface RentalService {
     void createNewRental(RentalDto rentalDto) throws ParseException;
 
     Rental saveRental(RentalDto rentalDto) throws ParseException;
+
+    List<RentalDto> getRentals();
+
+    RentalDto getRental(int id);
 }
